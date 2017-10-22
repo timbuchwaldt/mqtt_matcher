e652443ff440d5cee0bd18740cd94c1ea6f4267c
Tim Buchwaldt <tim@buchwaldt.ws>
2017-10-22 19:04:48 +0200

    Damn version requirements

--- mix.exs
+++ mix.exs
@@ -5,7 +5,7 @@ defmodule MQTTMatcher.MixProject do
     [
       app: :mqtt_matcher,
       version: "0.1.0",
-      elixir: "> 1.5",
+      elixir: "> 1.5.0",
       start_permanent: Mix.env() == :prod,
       deps: deps()
     ]
